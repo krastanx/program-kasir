3c906a48ea391e9fcfa9654d09af2d6f29ec8cca
krasta <hasisemp@gmail.com>
2025-10-07 14:40:50 +0700

    babi di rubah

--- app/Views/index.php
+++ app/Views/index.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Login Kasir</title>
+  <title>Login babi</title>
   
   <!-- CSS Inline untuk Form Login -->
   <style>
